fd60815b5f2a328c65891cba0e7338f56f5da98b
BenHenddrix <benhendrix1991@hotmail>
2021-08-18 13:22:53 +0200

    final commit i think

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\agow_\Desktop\Nieuwe programma's\.venv
+set VIRTUAL_ENV=C:\Users\agow_\Desktop\Games\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
